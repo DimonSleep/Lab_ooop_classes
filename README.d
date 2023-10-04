de882fc400819f98e1d2e0a3e3a41e4c011aac23
Dumitru Andoni <62153515+DimonSleep@users.noreply.github.com>
2023-10-04 19:25:29 +0300

    iuliana

---
+++ README.d
@@ -0,0 +1,13 @@
+#University Management System
+
+*create student class
+  *define fields
+  *define methods
+
+*create faculty class
+  *define fields
+  *define methods
+
+*create a program loop
+  *take inout
+  *display loop
